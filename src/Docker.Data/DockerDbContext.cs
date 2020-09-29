33aa521fa085f6cc522d9c1187caad886eee1222
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-09-29 22:21:55 +0200

    Add data seed

--- src/Docker.Data/DockerDbContext.cs
+++ src/Docker.Data/DockerDbContext.cs
@@ -17,5 +17,14 @@ namespace Docker.Data
         public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(_configuration.GetConnectionString("Database"));
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Product 1", Description = "First product", Price = 10, CreatedAt = DateTimeOffset.Now, UpdatedAt = DateTimeOffset.Now },
+                new Product { Id = 2, Name = "Product 2", Description = "Second product", Price = 15, CreatedAt = DateTimeOffset.Now, UpdatedAt = DateTimeOffset.Now },
+                new Product { Id = 3, Name = "Product 3", Description = "Third product", Price = 20, CreatedAt = DateTimeOffset.Now, UpdatedAt = DateTimeOffset.Now }
+            );
+        }
     }
 }
