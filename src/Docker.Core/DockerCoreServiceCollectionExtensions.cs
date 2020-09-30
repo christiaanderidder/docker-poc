9b75dfe7e6f30b5a5bb0ebdbaf41f388b4e5a1b4
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-09-30 14:38:50 +0200

    Add MassTransit with RabbitMQ

---
+++ src/Docker.Core/DockerCoreServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Docker.Core.Configuration;
+using MassTransit;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Docker.Core
+{
+    public static class DockerCoreServiceCollectionExtensions
+    {
+        public static void AddDockerCore(this IServiceCollection services)
+        {
+            
+        }
+
+        public static void AddAndConfigureMassTransit(this IServiceCollection services, IConfiguration config, Action<IServiceCollectionBusConfigurator> configure)
+        {
+            services.AddMassTransit(cfg =>
+            {
+                var rabbitMqConfig = config.GetSection("RabbitMq").Get<RabbitMqConfiguration>();
+                cfg.SetKebabCaseEndpointNameFormatter();
+
+                cfg.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(rabbitMqConfig.Host);
+
+                    // Automatically configure endpoints for all consumers
+                    cfg.ConfigureEndpoints(context);
+                });
+
+                // Allows the caller to set publishers and consumers
+                configure(cfg);
+            });
+
+            services.AddMassTransitHostedService();
+        }
+    }
+}
--- src/Docker.Worker/Program.cs
+++ src/Docker.Worker/Program.cs
@@ -1,24 +1,46 @@
+using Docker.Core;
+using Docker.Worker.Consumers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Docker.Worker
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateHostBuilder(args);
+
+            if (isService)
+            {
+                var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                builder = isWindows ? builder.UseWindowsService() : builder.UseSystemd();
+
+                await builder.Build().RunAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
+            {
+                services.AddAndConfigureMassTransit(hostContext.Configuration, (cfg) =>
                 {
-                    services.AddHostedService<Worker>();
                 });
+
+                services.AddHostedService<Worker>();
+            });
+        }
     }
 }
