99c6404e1041c5e312e5f8e280e9710c28390a95
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-10-08 21:58:26 +0200

    Fix HTTPS when running inside Kubernetes

--- src/Docker.Web/Startup.cs
+++ src/Docker.Web/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Docker.Web
@@ -44,6 +47,14 @@ namespace Docker.Web
             });
             services.AddRazorPages();
 
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                // Kubernetes service IP ranges
+                options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("10.0.0.0"), 8));
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardLimit = 2;
+            });
+
             services.AddAndConfigureMassTransit(Configuration);
         }
 
@@ -51,6 +62,8 @@ namespace Docker.Web
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             UpdateDatabase(app);
+
+            app.UseForwardedHeaders();
 
             if (env.IsDevelopment())
             {
