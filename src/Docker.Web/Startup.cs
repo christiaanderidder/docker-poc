065260b6f56c7e41f0d5bce07ff9ad3b7a488824
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-10-09 22:44:19 +0200

    Show the products page by default

--- src/Docker.Web/Startup.cs
+++ src/Docker.Web/Startup.cs
@@ -94,7 +94,7 @@ namespace Docker.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
