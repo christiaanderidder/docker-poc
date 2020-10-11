54c5c884abc829f094c018abeebb67d1f07ff371
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-10-11 19:33:30 +0200

    Add basic IdentityService implementation

---
+++ src/Web/Docker.OAuth/InMemoryConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Docker.OAuth
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources() => new[]
+        {
+            new ApiResource("product", "Product")
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes() => new[]
+        {
+            new ApiScope("product.read", "Read Product"),
+            new ApiScope("product.write", "Write Product")
+        };
+
+        public static IEnumerable<Client> ApiClients() => new[]
+        {
+            new Client()
+            {
+                ClientId = "testclient",
+                ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                AllowedScopes = new [] { "product.read", "product.write" },
+            }
+        };
+
+        public static IEnumerable<TestUser> Users() => new[]
+        {
+            new TestUser()
+            {
+                SubjectId = "1",
+                Username = "test@user.com",
+                Password = "password"
+            }
+        };
+    }
+}
--- src/Web/Docker.OAuth/Startup.cs
+++ src/Web/Docker.OAuth/Startup.cs
@@ -1,3 +1,4 @@
+using Docker.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -5,17 +6,32 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Docker.OAuth
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddSigningCredential(new X509Certificate2(Path.Combine(AppConfiguration.GetConfigPath(_env), "docker-poc-oauth.pfx")))
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.ApiClients())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources())
+                .AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +43,8 @@ namespace Docker.OAuth
             }
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
