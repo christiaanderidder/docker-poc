5879c869b7fb9059f3ef85bcfa9e0960c84e60d8
Christiaan de Ridder <ik@christiaanderidder.nl>
2020-10-11 19:28:57 +0200

    Add a volume mount for loading config files when using VS

--- src/Shared/Docker.Core/AppConfiguration.cs
+++ src/Shared/Docker.Core/AppConfiguration.cs
@@ -16,18 +16,26 @@ namespace Docker.Core
 #if DEBUG
             // For local builds, the config file will be loaded directly from the shared location
             // as it is linked virtually in the project file.
-            // Debug builds running in docker have the solution dir mounted to /src
-
-            // Detect docker. See: https://www.hanselman.com/blog/DetectingThatANETCoreAppIsRunningInADockerContainerAndSkippableFactsInXUnit.aspx
-            var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
-
-            var sharedFolder = isDocker ? "/src/" : Path.Combine(env.ContentRootPath, "../../");
+            // Docker compose mounts this folder under /root/.config
+            var isDocker = IsDocker();
+            var sharedFolder = GetConfigPath(env);
 
             // Disable reload on change, for some reason this is very slow on WSL
             // https://github.com/dotnet/runtime/issues/27272
             config.AddJsonFile(Path.Combine(sharedFolder, "appsettings.json"), optional: false, reloadOnChange: !isDocker);
             config.AddJsonFile(Path.Combine(sharedFolder, $"appsettings.{env.EnvironmentName}.json"), optional: false, reloadOnChange: !isDocker);
 #endif
+        }
+
+        public static bool IsDocker()
+        {
+            // Detect docker. See: https://www.hanselman.com/blog/DetectingThatANETCoreAppIsRunningInADockerContainerAndSkippableFactsInXUnit.aspx
+            return Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+        }
+
+        public static string GetConfigPath(IHostEnvironment env)
+        {
+            return IsDocker() ? "/root/.config" : Path.Combine(env.ContentRootPath, "../../../config");
         }
     }
 }
